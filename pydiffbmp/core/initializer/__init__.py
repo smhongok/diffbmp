64a6a04e36dd3b5591c213402dcf86b92c4f0b84
smhongok <smhongok@gmail.com>
2026-01-07 04:33:41 +0000

    feat: designated initializer

--- pydiffbmp/core/initializer/__init__.py
+++ pydiffbmp/core/initializer/__init__.py
@@ -6,10 +6,12 @@ from pydiffbmp.core.initializer.base_initializer import BaseInitializer
 from pydiffbmp.core.initializer.random_initializater import RandomInitializer
 from pydiffbmp.core.initializer.svgsplat_initializater import StructureAwareInitializer
 from pydiffbmp.core.initializer.sequnetial_initializater import SequentialInitializer
+from pydiffbmp.core.initializer.designated_initializer import DesignatedInitializer
 
 __all__ = [
     'BaseInitializer',
     'RandomInitializer',
     'StructureAwareInitializer',
     'SequentialInitializer',
+    'DesignatedInitializer',
 ]
